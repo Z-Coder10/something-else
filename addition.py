d8ff42144e4a2b761041430578e22f2158aaefc4
Z-Coder10 <adetomiwaomololu@gmail.com>
2025-08-06 19:59:40 +0100

    additon

---
+++ addition.py
@@ -0,0 +1,7 @@
+num1 = int(input("Please enter a number to add:"))
+
+num2 = int(input("Please enter a second number to add to the first number:"))
+
+num3 = num1 - num2
+
+print("The answer is",num3)
